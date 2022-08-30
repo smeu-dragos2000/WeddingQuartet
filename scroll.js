baf99b05029a653d273830e48433035f02791ace
smeu-dragos2000 <56107332+smeu-dragos2000@users.noreply.github.com>
2022-08-30 12:57:12 +0300

    first commitment

---
+++ scroll.js
@@ -0,0 +1,61 @@
+// Logo Scroll
+let logo = document.getElementById("logo-header");
+const scrollEffect = () => {
+  const distance = window.scrollY;
+  let scale = 1 * 8 / Math.sqrt(distance);
+  if (scale < 1) {
+    logo.style.transform = `translatey(${distance * 0.7}px)  scale(${scale})`;
+  }
+  else {
+    logo.style.transform = `translatey(${distance * 0.7}px)  scale(1)`;
+  }
+}
+const fadeIn = () => {
+  logo.classList.add("show")
+}
+window.addEventListener("scroll", scrollEffect);
+fadeIn();
+
+// Active Class
+const links = document.querySelectorAll(".nav-item");
+const ActiveMenu = () => {
+  let currentSection = '';
+
+  sections.forEach( section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= (sectionTop - sectionHeight/3)) {
+      currentSection = section.getAttribute('id');
+    }
+  })
+
+  links.forEach(li => {
+    li.classList.remove('active');
+    if (li.classList.contains(currentSection)) {
+      li.classList.add('active');
+    }
+    console.log(li)
+  })
+
+}
+// Active Class
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.onscroll = () => {
+
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 190;
+        let height = section.offsetHeight;
+        let sectionHeight = section.clientHeight;
+        let id = section.getAttribute('id')
+
+        if (top >= offset && top < offset + sectionHeight) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav ul li a[href*=' + id + ']').classList.add('active')
+            });
+        };
+    }); 
+}
---
+++ swiper.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper3 = new Swiper(".mySwiper3", {
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
